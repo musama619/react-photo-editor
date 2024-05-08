8fc9b93580eacb003d5531438e4916569f4a7877
musama619 <musama6191@gmail.com>
2024-05-09 00:00:21 +0500

    feat: add photo panning feature

--- src/tests/ReactPhotoEditor.test.tsx
+++ src/tests/ReactPhotoEditor.test.tsx
@@ -1,8 +1,8 @@
-import { cleanup, render, screen } from "@testing-library/react";
-import { afterEach, describe, expect, it, vi } from "vitest";
-import { ReactPhotoEditor } from "../components/ReactPhotoEditor";
-import '@testing-library/jest-dom/vitest'
-import * as matchers from '@testing-library/jest-dom/matchers'
+import { cleanup, render, screen } from '@testing-library/react';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import { ReactPhotoEditor } from '../components/ReactPhotoEditor';
+import '@testing-library/jest-dom/vitest';
+import * as matchers from '@testing-library/jest-dom/matchers';
 
 expect.extend(matchers);
 afterEach(() => {
@@ -14,60 +14,44 @@ global.fetch = vi.fn();
 global.URL.createObjectURL = vi.fn();
 global.URL.revokeObjectURL = vi.fn();
 
+describe('ReactPhotoEditor Component', () => {
+	const fileData = new File(['test content'], 'test.png', { type: 'image/png' });
+	const onSaveImage = vi.fn();
 
-describe("ReactPhotoEditor Component", () => {
+	it('should render component correctly', () => {
+		render(<ReactPhotoEditor file={fileData} onSaveImage={onSaveImage} open={true} />);
 
-    const fileData = new File(["test content"], "test.png", { type: "image/png" });
-    const onSaveImage = vi.fn();
+		const canvas = screen.getByTestId('image-editor-canvas');
+		expect(canvas).toBeInTheDocument();
+	});
+	it('should render rotate div correctly', () => {
+		render(
+			<ReactPhotoEditor file={fileData} onSaveImage={onSaveImage} open={true} allowRotate={true} />
+		);
 
-    it("should render component correctly", () => {
-        render(<ReactPhotoEditor file={fileData} onSaveImage={onSaveImage} open={true} />);
+		const rotate = screen.getByText('Rotate:');
+		expect(rotate).toBeInTheDocument();
+	});
+	it('should render flip buttons', () => {
+		render(
+			<ReactPhotoEditor file={fileData} onSaveImage={onSaveImage} open={true} allowFlip={true} />
+		);
 
-        const canvas = screen.getByTestId("image-editor-canvas");
-        expect(canvas).toBeInTheDocument();
-    });
-    it("should render rotate div correctly", () => {
-        render(<ReactPhotoEditor file={fileData} onSaveImage={onSaveImage} open={true} allowRotate={true} />);
+		const flipButtons = screen.getByTestId('flip-btns');
+		expect(flipButtons).toBeInTheDocument();
+	});
+	it('should render zoom buttons', () => {
+		render(
+			<ReactPhotoEditor file={fileData} onSaveImage={onSaveImage} open={true} allowZoom={true} />
+		);
 
-        const rotate = screen.getByText("Rotate:");
-        expect(rotate).toBeInTheDocument();
-    });
-    it("should render flip buttons", () => {
-        render(
-            <ReactPhotoEditor
-                file={fileData}
-                onSaveImage={onSaveImage}
-                open={true}
-                allowFlip={true}
-            />
-        );
+		const zoomButtons = screen.getByTestId('zoom-btns');
+		expect(zoomButtons).toBeInTheDocument();
+	});
+	it('should not render component if open == false', () => {
+		render(<ReactPhotoEditor file={fileData} onSaveImage={onSaveImage} open={false} />);
 
-        const flipButtons = screen.getByTestId("flip-btns");
-        expect(flipButtons).toBeInTheDocument();
-    });
-    it("should render zoom buttons", () => {
-        render(
-            <ReactPhotoEditor
-                file={fileData}
-                onSaveImage={onSaveImage}
-                open={true}
-                allowZoom={true}
-            />
-        );
-
-        const zoomButtons = screen.getByTestId("zoom-btns");
-        expect(zoomButtons).toBeInTheDocument();
-    });
-    it("should not render component if open == false", () => {
-        render(
-            <ReactPhotoEditor
-                file={fileData}
-                onSaveImage={onSaveImage}
-                open={false}
-            />
-        );
-
-        const zoomButtons = screen.queryByTestId("photo-editor-main");
-        expect(zoomButtons).not.toBeInTheDocument();
-    });
+		const zoomButtons = screen.queryByTestId('photo-editor-main');
+		expect(zoomButtons).not.toBeInTheDocument();
+	});
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,31 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        dts({
-            rollupTypes: true,
-        }),
-    ],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, "src/index.tsx"),
-            name: "ReactPhotoEditor",
-            formats: ["es"],
-            fileName: (format) => `react-photo-editor.${format}.js`,
-        },
-        rollupOptions: {
-            external: ["react", "react-dom"],
-            output: {
-                globals: {
-                    react: "React",
-                },
-            },
-        },
-        minify: true,
-    },
+	plugins: [
+		react(),
+		dts({
+			rollupTypes: true,
+		}),
+	],
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.tsx'),
+			name: 'ReactPhotoEditor',
+			formats: ['es'],
+			fileName: (format) => `react-photo-editor.${format}.js`,
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom'],
+			output: {
+				globals: {
+					react: 'React',
+				},
+			},
+		},
+		minify: true,
+	},
 });
